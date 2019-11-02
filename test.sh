6cf74c03fd9e0197d2f62d15da223649e23b1058
korven48 <korven48@gmail.com>
2019-11-02 12:19:31 +0100

    test.sh creates a result file and asks for name of the files to be executed. Also .gitignore was added

--- test.sh
+++ test.sh
@@ -1,16 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+attempt_file=$1
+correct_file=$2
 
 for (( i=0; i<10; i++ ))
 do
     echo Running test $i
     echo ---------------
     ./generator.py > test
-    attemp=$(./try.py < test)
-    true=$(./correct.py < test)
+    attemp=$(./$attempt_file < test)
+    true=$(./$correct_file < test)
     if [[ $attemp != $true ]]
     then
-        $(./try.py < test > attempt)
-        $(./correct.py < test > correct)
-        break
+        ./$attempt_file < test > attempt
+        ./$correct_file < test > correct
+        echo "Test $i has failed" > result
+        exit
     fi
 done
+
+echo "All test passed" > result
