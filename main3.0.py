679eae3232d0439fcfc9109a1474fe4ff52dd3f6
RoDiHackProg <rodihackpro@gmail.com>
2019-11-19 22:53:23 +0100

    Little change

--- main3.0.py
+++ main3.0.py
@@ -9,17 +9,12 @@ class NewProblem:
         problem = LabelFrame(problems, text=f'Problem {self.num}', font=('Consolas', 15),height=90, bd=0)
         problem.pack(fill=X, side=TOP, padx=20, pady=10)
 
-        button = Button(problem, text='Submit', command=self.change, width=15, font='Consolas', relief=RAISED)
+        button = Button(problem, text='Submit', command=lambda: self.result.set('hi'), width=15, font='Consolas', relief=RAISED)
         button.pack(side=LEFT, padx=10, pady=10)
 
         resultLabel =  Label(problem, textvariable=self.result, font=('Consolas', 10), width=100, bg='grey80', height=2, anchor=W, padx=10)
         resultLabel.pack(side=LEFT, padx=30)
         self.result.set('Tests results')
-
-    def change(self):
-        self.result.set('hi')
-        
-
 
 window = Tk()
 window.geometry('1000x500')
