d53009eb0612474fe78a5bb02bd1110918dfd6e6
korven48 <korven48@gmail.com>
2019-12-01 13:50:44 +0100

    Added permissions to all files

--- newproblem.py
+++ newproblem.py
@@ -2,25 +2,18 @@
 
 from tkinter import *
 import os
-import json
 
 CURRENT_DIR = os.getcwd()
 problems_path = CURRENT_DIR + '/Problems/' #Dirección de carpeta con problemas
 attempts_path = CURRENT_DIR + '/Attempts/'
 generators_path = CURRENT_DIR + '/Generators/'
-username = 'Pepe Palotes'
-points_ = 0
 
-os.system('chmod 744 -R .') 
-# Da permisos de ejecucción, lectura y escritura a
-# todos los programas y carpetas contenidos en el directorio local
- 
 class NewProblem():
     def __init__(self, num):
+        global problems
         self.num = num
         self.state = 'Test results'
         self.message = StringVar()
-
         self.solution = problems_path + f'solution_{self.num}.py'
         self.generator = generators_path + f'generator_{self.num}.py'
 
@@ -36,10 +29,10 @@ class NewProblem():
     
     def check(self):
         global points_
-        self.attempt = attempts_path + f'problema_{self.num}.py'
-        os.system(f'chmod 744 {self.attempt}')
 
-        self.message.set(f'Running test {self.num}')
+        #self.message.set('Running tests') # {self.num}
+        self.attempt = attempts_path + f'problema_{self.num}.py'
+
         os.system(f'./test.sh {self.attempt} {self.solution} {self.generator}')
         with open('result', 'r') as f:
             result = f.read().replace('\n', '')
@@ -52,36 +45,3 @@ class NewProblem():
         else:
             self.state = result
             self.message.set(result)
-
-
-window = Tk()
-window.geometry('1000x500')
-
-#-------------------------------HEADER-------------------------------------
-
-header = LabelFrame(window, height=80, bd=0)#bd=5
-header.pack(fill=X, side=TOP)
-
-appName = Label(header, text='CodeTester', font=('Consolas', 40))
-appName.pack(side=LEFT, padx=20)
-
-name = Label(header, text=username, bd=5, font=('Consolas', 14), anchor=E)
-name.pack(side=BOTTOM, fill=BOTH, padx= 20)
-
-pointsWord =  Label(header, text='points', font=('Consolas', 20))
-pointsWord.pack(side=RIGHT, padx=20)
-
-points = StringVar()
-counter =  Label(header, textvariable=points, font=('Consolas', 40))
-counter.pack(side=RIGHT)
-points.set(points_)
-
-#-------------------------------PROBLEMS-------------------------------------
-
-problems = LabelFrame(window, height=900, bd=0)
-problems.pack(fill=BOTH, side=TOP)
-
-for i in range(1, 2 + 1): # Ese 2 debería ser la cantidad de problemas
-    NewProblem(i)
-
-window.mainloop()
